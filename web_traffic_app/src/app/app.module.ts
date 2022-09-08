aeee29019ed1116a95796ca2590ed29a89b7da64
Sheryar <sheryarstorm1033@gmail.com>
2022-09-08 21:09:13 +0500

    header footer

--- web_traffic_app/src/app/app.module.ts
+++ web_traffic_app/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainpageComponent } from './arabic/components/mainpage/mainpage.component';
+import { SignupComponent } from './arabic/components/signup/signup.component';
+import { SigninComponent } from './arabic/components/signin/signin.component';
+import { HomeComponent } from './arabic/components/home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    SignupComponent,
+    SigninComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
